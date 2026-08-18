f3b285e09f3adc376a9c6a5671904f705a72669a
Aldahir Mayorga <samg.25sep@gmail.com>
2026-08-17 18:27:08 -0600

    [ADD] Se añade componente CalorieTracker para mostrar los totales de Calorias consumidas. quedamas y un balance final

---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,57 @@
+import {useMemo} from "react";
+import type {ActivityT} from "../types";
+import {CalorieDisplay} from "./utils/CalorieDisplay";
+
+type CalorieTrackerPropsT = {
+  activities: ActivityT[];
+};
+
+export const CalorieTracker = ({activities}: CalorieTrackerPropsT) => {
+  //contadores
+  const caloriesConsumed = useMemo(() => {
+    return activities.reduce(
+      (total, activity) =>
+        activity.category === 1 ? total + activity.calories : total,
+      0,
+    );
+  }, [activities]);
+
+  const caloriesBurned = useMemo(() => {
+    return activities.reduce(
+      (total, activity) =>
+        activity.category === 2 ? total + activity.calories : total,
+      0,
+    );
+  }, [activities]);
+
+  const netCalories = useMemo(() => {
+    return caloriesConsumed - caloriesBurned;
+  }, [activities]);
+  return (
+    <>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de Calorias
+      </h2>
+
+      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+        <CalorieDisplay
+          text={"Consumidas"}
+          type={"consumed"}
+          calories={caloriesConsumed}
+        />
+
+        <CalorieDisplay
+          text={"Ejercicio"}
+          type={"burned"}
+          calories={caloriesBurned}
+        />
+
+        <CalorieDisplay
+          text={"Total de Calorias"}
+          type={""}
+          calories={netCalories}
+        />
+      </div>
+    </>
+  );
+};
---
+++ src/components/utils/CalorieDisplay.tsx
@@ -0,0 +1,32 @@
+type CalorieDisplayPropsT = {
+  calories: number;
+  text: string;
+  type: string;
+};
+export const CalorieDisplay = ({
+  calories,
+  text,
+  type,
+}: CalorieDisplayPropsT) => {
+  const getColorByType = (type: string): string => {
+    switch (type) {
+      case "consumed":
+        return `yellow`;
+
+      case "burned":
+        return `red`;
+
+      default:
+        return "while";
+    }
+  };
+
+  return (
+    <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+      <span className={`font-black text-6xl text-${getColorByType(type)}-500`}>
+        {calories}
+      </span>{" "}
+      {text}
+    </p>
+  );
+};
